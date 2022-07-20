f94a06a114065bd4877f55af9a6054dcd5f72e35
USER <USER@DESKTOP-435L2RI>
2022-07-20 12:46:48 +0530

    first upload

---
+++ selenium_git/src/test/java/selenium_git/Practicetest.java
@@ -0,0 +1,18 @@
+package selenium_git;
+
+import org.testng.annotations.Test;
+
+public class Practicetest {
+	@Test
+	public void demo() {
+		System.out.println("test-->test2");
+		System.out.println("sample for git");
+	}
+	public void demo2() {
+		System.out.println("test2-->test3");
+		System.out.println("sample to git hub");
+		System.out.println("eclpise to switch branch in git");
+		System.out.println("thank you");
+	}
+
+}
---
+++ selenium_git/src/test/java/selenium_git/practice2test.java
@@ -0,0 +1,14 @@
+package selenium_git;
+
+import org.testng.annotations.Test;
+
+public class practice2test {
+	@Test
+	public void demo1() {
+		System.out.println("test-->test3");
+	}
+	public void demo2() {
+		System.out.println("thank you");
+	}
+
+}
